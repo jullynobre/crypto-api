17306d94ef1713cbf119c13102a61a51e284f16f
Diuli Nobre <jullynobre1@gmail.com>
2021-03-14 23:51:05 -0300

    Uncomment CORSMiddleware

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 		cryptoGroup.POST("/btc", crypto.UpdateCryptoEndpoint)
 	}
 
-	// r.Use(CORSMiddleware())
+	r.Use(CORSMiddleware())
 
 	r.Run(":8000")
 }
@@ -41,20 +41,20 @@ func AuthMidleware() gin.HandlerFunc {
 	}
 }
 
-// CORSMiddleware ...
-// func CORSMiddleware() gin.HandlerFunc {
-// 	return func(c *gin.Context) {
-// 		c.Writer.Header().Set("Content-Type", "application/json")
-// 		c.Writer.Header().Set("Access-Control-Allow-Origin", c.Request.Header.Get("Origin"))
-// 		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
-// 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
-// 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Max")
-// 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+// CORSMiddleware allows CORS requests
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Content-Type", "application/json")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", c.Request.Header.Get("Origin"))
+		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Max")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 
-// 		if c.Request.Method == "OPTIONS" {
-// 			c.AbortWithStatus(200)
-// 		} else {
-// 			c.Next()
-// 		}
-// 	}
-// }
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(200)
+		} else {
+			c.Next()
+		}
+	}
+}
